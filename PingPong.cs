5d15de256fa98417b95e6802a720f33d1a62cbca
Student <example@example.com>
2019-12-09 10:52:26 -0800

    create a s-sharp pingpong game

---
+++ PingPong.cs
@@ -0,0 +1,31 @@
+using System;
+
+class PingPong
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a number.");
+        string userInput = Console.ReadLine();
+        int inputNumber = int.Parse(userInput);
+
+        for (int i = 1; i < inputNumber; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("ping-pong");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("pong");
+            } 
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("ping");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }    
+}
